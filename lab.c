6de69f9b9d4f13dead273abb6e40610783feb899
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2020-02-02 13:14:19 +0100

    Update lab.c

--- lab.c
+++ lab.c
@@ -6,8 +6,18 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Bhanu Kiran Chaluvadi");
 MODULE_DESCRIPTION("Simple test kernel modules");
 
+static int number = 99;
+static char* word = "universal";
+
+module_param(number, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(number, "A interger");
+module_param(word, charp, 0000);
+MODULE_PARM_DESC(word, "A character string");
+
 int my_init_module(void) {
     printk("in init module \n");
+    printk(KERN_INFO "Number: %d\n", number);
+    printk(KERN_INFO "Word string: %s\n", word);
     return 0;
 }
 
